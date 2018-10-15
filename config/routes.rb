f0f58ffda086e9a3163d93482f7308aca920ccf2
Jessica Phillips <jlphilli22@ymail.com>
2018-10-15 09:15:52 -0400

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'sales/create'
+  get 'sales/show'
+  get 'authors/index'
+  get 'authors/show'
+  get 'books/index'
+  get 'books/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
