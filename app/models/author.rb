3752185bb3316e231923c4d37ae625a691003a01
Jessica Phillips <jlphilli22@ymail.com>
2018-10-18 15:13:55 -0400

    reconfigured routes, added mailcatcher

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,8 @@
 class Author < ApplicationRecord
 	has_many :books
 
+def name
+	self[:first_name] + " " + self[:last_name]
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
- root to: "books#index"
+ root to: "home#index"
   resources :books, only: :show do
     resources :sales, only: [:new, :create]
   end
+
+  resources :authors
+
+  get "/books", to: "books#index" 
 
   get "/sales", to: "sales#index"
 
