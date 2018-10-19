26b0f1acde8ed3b80be08ba0d4349c5a02e774ae
Jessica Phillips <jlphilli22@ymail.com>
2018-10-19 16:45:12 -0400

    changed books and authors migrations, null false

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   get "/sales", to: "sales#index"
 
-  get "styleguide", to: "styleguide#index"
+  get "/styleguide", to: "styleguide#index"
+
 end
 
--- db/migrate/20181014175334_create_books.rb
+++ db/migrate/20181014175334_create_books.rb
@@ -1,8 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.string :title, null :false
-      t.text :content, null :false
+      t.string :title, null: :false
+      t.text :content, null: :false
       t.integer :author_id
 
       t.timestamps
