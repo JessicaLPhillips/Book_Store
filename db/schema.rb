bf9b6e17c07c5bf88a987d1a2f7f48b549e861a9
Jessica Phillips <jlphilli22@ymail.com>
2018-10-21 09:08:41 -0400

    accidentally deleted user model

--- db/schema.rb
+++ db/schema.rb
@@ -37,18 +37,4 @@ ActiveRecord::Schema.define(version: 2018_10_16_184152) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "admin", default: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
 end
